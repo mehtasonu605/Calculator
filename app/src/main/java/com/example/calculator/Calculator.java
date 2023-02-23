a9b40a4cf41a4549259c5dd010bac61d2511d2d0
sonu <mehtasonu605@gmail.com>
2023-02-23 17:21:51 +0530

    Design completed

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,36 @@
+package com.example.calculator;
+
+
+//Class to perform calculation
+public class Calculator {
+
+    // Method for Adding two values
+    double AddValues(double value1, double value2){
+        double Sum = 0;
+        Sum = value1 + value2;
+        return Sum;
+    }
+
+    // Method to subtract two values
+    double SubtractValues(int value1, double value2){
+        double Difference;
+        Difference = value1 - value2;
+        return Difference;
+    }
+
+    // Method to Multiply two values
+    double MultiplyValues(double value1, double value2){
+        double Product;
+        Product = (value1 * value2);
+        return Product;
+    }
+
+    // Method to perform division between two values
+
+    double DivideValues(double value1, double value2){
+        double Quotient;
+        Quotient = (double) (value1/value2);
+        return Quotient;
+    }
+
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button_1,button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_0;
+    Button buttonDivide, buttonMultiply, buttonAdd, buttonSubtract, buttonDecimal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_0 = findViewById(R.id.btn0);
+        button_1 = findViewById(R.id.btn1);
+        button_2 = findViewById(R.id.btn2);
+        button_3 = findViewById(R.id.btn3);
+        button_4 = findViewById(R.id.btn4);
+        button_5 = findViewById(R.id.btn5);
+        button_6 = findViewById(R.id.btn6);
+        button_7 = findViewById(R.id.btn7);
+        button_8 = findViewById(R.id.btn8);
+        button_9 = findViewById(R.id.btn9);
+        buttonMultiply = findViewById(R.id.btnMultiply);
+        buttonAdd = findViewById(R.id.btnAddition);
+        buttonDivide = findViewById(R.id.btnDivide);
+        buttonSubtract = findViewById(R.id.btnSubtract);
+        buttonDecimal = findViewById(R.id.btnDecimal);
     }
 }
